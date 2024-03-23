2f4621afe0119aa196d8ec0117713cd9ed505554
fellipySaldanha <fellipy.saldanha@outlook.com>
2024-03-22 21:13:49 -0300

    db name

--- src/domain/aggregates/appointment/gateway/DynamoGateway.ts
+++ src/domain/aggregates/appointment/gateway/DynamoGateway.ts
@@ -8,7 +8,7 @@ import TimeSheetRecord from "../entities/TimeSheetRecord";
 export default class DynamoGateway implements IGateway{
 
     private dynamodb: DynamoDBDocument;
-    private table = process.env.DYNAMODB_NAME;
+    private table = 'teste';
     
     constructor() {
       this.dynamodb = DynamoDBDocument.from(
